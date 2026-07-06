894b5739495d7696d5c1a3cc1417f171a58d3729
Timur K <timurkakandzo@gmail.com>
2026-07-06 14:00:16 +0300

    final1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import { useState } from 'react';
+import cn from 'classnames';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
@@ -9,36 +10,74 @@ export const tabs = [
   { id: 'tab-3', title: 'Tab 3', content: 'Some text 3' },
 ];
 
-export const App = () => (
-  <div className="section">
-    <h1 className="title">Selected tab is Tab 1</h1>
+export const App = () => {
+  const [selectedTab, setSelectedTab] = useState('tab-1');
 
-    <div data-cy="TabsComponent">
-      <div className="tabs is-boxed">
-        <ul>
-          <li className="is-active" data-cy="Tab">
-            <a href="#tab-1" data-cy="TabLink">
-              Tab 1
-            </a>
-          </li>
+  return (
+    <div className="section">
+      <h1 className="title">
+        Selected tab is {tabs.find(tab => tab.id === selectedTab)?.title}
+      </h1>
 
-          <li data-cy="Tab">
-            <a href="#tab-2" data-cy="TabLink">
-              Tab 2
-            </a>
-          </li>
+      <div data-cy="TabsComponent">
+        <div className="tabs is-boxed">
+          <ul>
+            {/* Вкладка 1 */}
+            <li
+              data-cy="Tab"
+              className={cn({ 'is-active': selectedTab === 'tab-1' })}
+            >
+              <a
+                href="#tab-1"
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  setSelectedTab('tab-1');
+                }}
+              >
+                Tab 1
+              </a>
+            </li>
 
-          <li data-cy="Tab">
-            <a href="#tab-3" data-cy="TabLink">
-              Tab 3
-            </a>
-          </li>
-        </ul>
-      </div>
+            {/* Вкладка 2 */}
+            <li
+              data-cy="Tab"
+              className={cn({ 'is-active': selectedTab === 'tab-2' })}
+            >
+              <a
+                href="#tab-2"
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  setSelectedTab('tab-2');
+                }}
+              >
+                Tab 2
+              </a>
+            </li>
 
-      <div className="block" data-cy="TabContent">
-        Some text 1
+            <li
+              data-cy="Tab"
+              className={cn({ 'is-active': selectedTab === 'tab-3' })}
+            >
+              <a
+                href="#tab-3"
+                data-cy="TabLink"
+                onClick={event => {
+                  event.preventDefault();
+                  setSelectedTab('tab-3');
+                }}
+              >
+                Tab 3
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="block" data-cy="TabContent">
+          {tabs.find(tab => tab.id === selectedTab)?.content}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
